350201918080f208fd8222b14fdd38b72bf30fa1
guilhermedkdk <guilhermeperesmm@gmail.com>
2023-02-23 11:27:03 -0300

    updating files

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main(props) {
+    return (
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main--title">Curiosidades sobre o React</h1>
+            <ul className="main--facts">
+                <li>Foi lançado pela primeira vez em 2013</li>
+                <li>Foi originalmente criado por Jordan Walke</li>
+                <li>Tem mais de 100 mil estrelas no GitHub</li>
+                <li>É mantido pelo Facebook</li>
+                <li>Capacita milhares de aplicativos corporativos, incluindo aplicativos móveis</li>
+            </ul>
+        </main>
+    )
+}
